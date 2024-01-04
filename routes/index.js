9ea61f73b9799cc531ef2781d7dff085b56b2798
Drew Code <pekke100@gmail.com>
2024-01-04 01:44:19 +0000

    done with contact form

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CodeHub- Sahring code just got easier' });
+});
+
+router.get ("/about", function(req, res, next){
+  res.render("about", { title:"CodeHub -Sharing Code Just got easier"});
+
+});
+
+router.get("/contact", function(req, res, next){
+  res.render("contact", { title:" CodeHub -Sharing code just got easier"});
+
+})
+.post(function(req, res, next){
+  res.render("Thank you for contacting CodeHub", {title:"CodeHub - Sharing code just got better"});
+
 });
 
 module.exports = router;
