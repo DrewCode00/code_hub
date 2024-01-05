c39bd87ef3efdfab2bb79e6afba2d8e122cca8c1
Drew Code <pekke100@gmail.com>
2024-01-05 01:10:24 +0000

    Working on Register and Login page

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+import { ExpressValidatorOptions } from "./node_modules/express-validator/index.d";
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var ExpressValidator = require("express-validator");
+var bodyParser = require("body-parser");
+var express = require("express");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -16,11 +21,13 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(ExpressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+var config = require('../config');
+var transporter = nodemailer.createTransport(config.mailer);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,8 +20,49 @@ router.get("/contact", function(req, res, next){
 
 })
 .post(function(req, res, next){
-  res.render("Thank you for contacting CodeHub", {title:"CodeHub - Sharing code just got better"});
+  req.checkBody("name", "Invalid name").notEmpty();
+  req.checkBody("Email", "Invalid email").isEmail();
+  req.checkBody("message", "Empty message").notEmpty();
+  
+  var errors = req.validationErrors();
 
+  if(errors){
+    res.render("contact",{
+      title: "CodeHub -- Sharing Code just got better.",
+      name: req.body.name,
+      email: req.body.email,
+      message: req.body.message,
+      errorMessage: errors
+    });
+  }else{
+    var mailOptions={
+      from:'Codehub <no-reply@codehub.com>',
+      to:'demo.codehub@gmail.com',
+      Subject: 'You have got a new message from Visitor ✌️',
+
+
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        return console.log(error);
+
+      }
+      res.render("Thank you for contacting CodeHub", {title:"CodeHub - Sharing code just got better"});
+
+    })
+
+   
+  }
+
+  router.get('/login', function(req, res, next){
+    res.render('login', {title: 'Login your account'});
+
+  });
+
+  router.get('/register', function(req, res, next){
+    res.render('register', {title: 'Register a new account'});
+  });
+ 
 });
 
 module.exports = router;
