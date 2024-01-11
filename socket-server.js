eed9ad388a65fa0f0d883a959f5f1ccdf4176cf8
Drew Code <pekke100@gmail.com>
2024-01-11 01:04:54 +0000

    working on code sharing

--- socket-server.js
+++ socket-server.js
@@ -5,10 +5,23 @@ var SocketIO = require('socket.io');
 
 module.exports = function(server){
     var io = SocketIO(server);
+
+    io.on('connection', function(socket){
+        socket.on('joinRoom', function(data){
+            socket.room =data.room;
+            socket.join(data.room);
+        })
+    })
+
     io.onconnection('connection', function(socket){
         socket.on('chatMessage', function(data){
-            io.emit('chatMessage', data);
-            
+
+            io.to(socket.room).emit('chatMessage', data);
+
+        });
+
+        socket.on('disconnect', function(){
+            socket.leave(socket.room);
         })
     })
 }
--- views/model/task.js
+++ views/model/task.js
@@ -1,9 +1,46 @@
 var mongoose = require('mongoose');
 
 var taskSchema = new mongoose.Schema({
+content: String
+
+});
+
+var newTask = new taskSchema();
 
 
-})
+newTask.save(function( err, data){
+    if(err){
+        console.log(err);
+        res.render('error');
+    }else{
+        res.redirect('/task/' + data._data);
 
-module.exports = mongoose.model('TAsk', taskSchema);
+    }
+});
+
+router.get('/task/:id', function(req, res){
+    if(req.params.id){
+        Task.findOne({_id: req.params.id}, function(err, data){
+            if(err){
+                console.log('error');
+                res.render('error');
+
+            }
+
+            if (data){
+                res.render('task', {data: data, roomId: data.id});
+
+            }else{
+                res.render('error');
+            }
+        })
+    }else{
+        res.render('error');
+
+    }
+});
+
+module.exports =router;
+
+module.exports = mongoose.model('Task', taskSchema);
 
