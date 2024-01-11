d274ad726bfe5481b679cd9594993b89c24a44f5
Drew Code <pekke100@gmail.com>
2024-01-11 09:09:02 +0000

    finished the code editor

--- socket-server.js
+++ socket-server.js
@@ -1,13 +1,32 @@
 'use strict';
 
 var SocketIO = require('socket.io');
+var ot = require('ot');
 
+var roomList ={};
 
 module.exports = function(server){
+    var str = 'This is a Markdown heading \n\n' +
+    'var i=i + 1';    
+    
+    
     var io = SocketIO(server);
 
     io.on('connection', function(socket){
         socket.on('joinRoom', function(data){
+
+            if(!roomList[data.room]){
+                var socketIOserver = new ot.EditorSocketIOServer(str, [], data.room, function(socket, cb){
+
+                    cb(true);
+
+                });
+                roomList[data.room] =socketIOServer;
+            }
+
+            roomList[data.room].addClient(socket);
+            roomList[data.room].setName(socket, data.username);
+
             socket.room =data.room;
             socket.join(data.room);
         })
